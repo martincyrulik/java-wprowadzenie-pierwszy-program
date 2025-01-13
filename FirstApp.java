ac34a088bd94bb6e9d89d6e8da23e162366f9c62
martincyrulik <martincyrulik@gmail.com>
2025-01-13 21:12:04 +0100

    pierwszy program w Java

---
+++ FirstApp.java
@@ -0,0 +1,25 @@
+class FirstApp { 
+	public static void main(String args[]){
+		// pierwszy program
+		System.out.println("Dzisiaj jest pi�kna pogoda.\n");
+	
+
+		// zadanie 1
+		System.out.println("Pan pomidor wlaz� na tyczk�");
+		System.out.println("I przedrze�nia ogrodniczk�.");
+		System.out.println("\"Jak pan mo�e,");
+		System.out.println("Panie pomidorze?!\"");
+
+		System.out.println();
+
+		// zadanie 2
+		System.out.println("------------------------------");
+		System.out.println("Do biedronki przyszed� �uk,");
+		System.out.println("W okieneczko puk-puk-puk.");
+		System.out.println();
+		System.out.println("Panieneczka widzi �uka:");
+		System.out.println("\"Czego pan tu u mnie szuka?\"");
+		System.out.println("------------------------------");
+
+	}
+}
